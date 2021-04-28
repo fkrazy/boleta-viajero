43e6d9517b66105f79596b56202a7afe50d14703
frank orozco <frank@iqsoftware.com.gt>
2021-04-27 23:33:36 -0600

    set aduanas model

--- paises/admin.py
+++ paises/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from paises.models import Pais
+from paises.models import Pais, Aduana
 
 admin.site.register(Pais)
+admin.site.register(Aduana)
