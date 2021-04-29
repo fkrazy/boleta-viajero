99a33a548b8fb5359c31ba0b7600206631d8c0e5
frank orozco <frank@iqsoftware.com.gt>
2021-04-28 22:50:16 -0600

    fix models

--- bitacora/models.py
+++ bitacora/models.py
@@ -1,24 +1,27 @@
 
 from django.db import models
 
-from paises.models import Pais
+from paises.models import Pais, Aduana
+from transporte.models import transporte
+from viajeros.models import Viajero
 
 
 class bitacora (models.Model):
     accion = models.CharField(max_length=8, default="")
     fecha = models.DateField()
     motivo_viaje = models.CharField(max_length=20, default="")
-    #id_aduana = models.ForeignKey(aduanas, on_delete=models.CASCADE)
-    #id_viajero = models.ForeignKey(viajeros, on_delete=models.CASCADE)
-    #id_destino = models.ForeignKey(destinos, on_delete=models.CASCADE)
-    #id_transporte = models.ForeignKey(transportes, on_delete=models.CASCADE)
-    pais_procedencia = models.ForeignKey(Pais, verbose_name='pais', related_name="paises_bitacora", on_delete=models.PROTECT)
+    aduana = models.ForeignKey(Aduana, on_delete=models.CASCADE, null=True)
+    viajero = models.ForeignKey(Viajero, on_delete=models.CASCADE, null=True)
+    transporte = models.ForeignKey(transporte, on_delete=models.CASCADE, null=True)
+    pais_procedencia = models.ForeignKey(Pais, verbose_name='pais', related_name="paises_bitacora",
+                                         on_delete=models.PROTECT)
+    pais_destino = models.ForeignKey(Pais, verbose_name='pais', related_name="paises_destino", null=True,
+                                     on_delete=models.PROTECT)
+    direccion = models.CharField(max_length=70, default="")
     numero_familiares = models.IntegerField()
     cantidad_equipaje = models.IntegerField()
     cantidad_no_acompanado = models.IntegerField()
     dinero_mayor_diezmil = models.FloatField()
-    #id_moneda = models.ForeignKey(detalle_valores_monetarios, on_delete=models.CASCADE)
-    #id_material = models.ForeignKey(detalle_valors_monetarios, on_delete=models.CASCADE)
     animales_plantas_alimentos = models.FloatField()
     producto_quimico_sustancias = models.FloatField()
     mercancias_distintas = models.FloatField()
