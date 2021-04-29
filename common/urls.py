8aaf14de60da63a8fc5ea6a4cc00090c95f06f45
frank orozco <frank@iqsoftware.com.gt>
2021-04-29 01:43:19 -0600

    set tipo cambio banguat

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework import routers
 
-from common.views import TipoCambioView, GetGroups
+from common.views import TipoCambioView, GetGroups, MonedasBanguatView, TipoCambioMonedaView
 
 urlpatterns = [
     path('tipo-cambio/', TipoCambioView.as_view()),
+    path('monedas_banguat/', MonedasBanguatView.as_view()),
+    path('tipo_cambio_moneda/<int:id>', TipoCambioMonedaView.as_view()),
     path('groups/', GetGroups.as_view()),
 ]
--- common/views.py
+++ common/views.py
@@ -22,6 +22,47 @@ class TipoCambioView(APIView):
         return Response(response['CambioDolar']['VarDolar'][0]['referencia'])
 
 
+class MonedasBanguatView(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        wsdl = "http://www.banguat.gob.gt/variables/ws/TipoCambio.asmx?wsdl"
+        client = Client(wsdl)
+        response = client.service.VariablesDisponibles()
+        currency_list = []
+        for data in response["Variables"]["Variable"]:
+            currency_list.append(dict(moneda=data["moneda"], descripcion=data["descripcion"]))
+        return Response(currency_list)
+
+
+class TipoCambioMonedaView(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+
+    def get(self, request, format=None, **kwargs):
+        """
+        Return a list of all users.
+        """
+        wsdl = "http://www.banguat.gob.gt/variables/ws/TipoCambio.asmx?wsdl"
+        client = Client(wsdl)
+        response = client.service.Variables(kwargs.get("id", 1))
+        response = response["CambioDia"]["Var"][0]
+        response = dict(compra=response["compra"], venta=response["venta"])
+        return Response(response)
+
+
 class GetGroups(APIView):
     permission_classes = [IsAuthenticated]
 
