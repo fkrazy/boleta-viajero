4e70899f66a2feb267aa28d99f468d32754fbfff
frank orozco <frank@iqsoftware.com.gt>
2021-04-28 21:32:55 -0600

    set bitacors to monedas models

--- monedas/models.py
+++ monedas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from bitacora.models import bitacora
 
 
 class Moneda(models.Model):
@@ -14,7 +15,8 @@ class DetalleValoresMonetarios(models.Model):
     valor_dolares = models.DecimalField(decimal_places=2, max_digits=9)
     origen_fondos = models.TextField(max_length=255)
     destino_fondos = models.TextField(max_length=255)
-
+    bitacora = models.ForeignKey(bitacora, verbose_name='bitacora', related_name="detallesValoresMonetarios",
+                                 on_delete=models.PROTECT, null=True)
     moneda = models.ForeignKey(Moneda, verbose_name='moneda', related_name="monedas", on_delete=models.PROTECT)
     material = models.ForeignKey(MaterialPrecioso, verbose_name='material', related_name="materiales",
                                  on_delete=models.PROTECT)
@@ -24,3 +26,5 @@ class EquipajeExtra(models.Model):
     cantidad = models.IntegerField()
     descripcion = models.TextField(max_length=255)
     valor_dolares= models.DecimalField(decimal_places=2, max_digits=9)
+    bitacora = models.ForeignKey(bitacora, verbose_name='bitacora', related_name="equipajes_extra",
+                                 on_delete=models.PROTECT, null=True)
