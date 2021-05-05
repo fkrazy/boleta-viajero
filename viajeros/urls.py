97d91a65814782220e0226405499f77fd1e59bde
frank orozco <frank@iqsoftware.com.gt>
2021-05-05 05:23:27 -0600

    set viajeros por republica endpoint

--- viajeros/urls.py
+++ viajeros/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from viajeros.views import ViajerosFrecuentesView
+from viajeros.views import ViajerosFrecuentesView, ViajerosRepublicaView
 
 urlpatterns = [
     path('reporte/frecuentes/', ViajerosFrecuentesView.as_view()),
+    path('reporte/republica/', ViajerosRepublicaView.as_view()),
 ]
--- viajeros/views.py
+++ viajeros/views.py
@@ -6,6 +6,7 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from paises.models import Pais
 from viajeros.models import Viajero
 
 
@@ -34,4 +35,34 @@ class ViajerosFrecuentesView(APIView):
         """
         serializer = ViajeroFrecuenteSerializer(Viajero.objects.all(), many=True)
         return Response(serializer.data)
+
+
+class ViajeroRepublicaSerializer(serializers.ModelSerializer):
+    viajeros = serializers.SerializerMethodField()
+
+    def get_viajeros(self, obj):
+        return obj.aduanas__bitacora__viajero__count
+
+    class Meta:
+        model = Pais
+        fields = ["id", "nombre_pais", "viajeros"]
+
+
+class ViajerosRepublicaView(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        paises = Pais.objects.filter(aduanas__isnull=False)
+        paises = paises.annotate(Count("aduanas__bitacora__viajero"))
+        serializer = ViajeroRepublicaSerializer(paises, many=True)
+        return Response(serializer.data)
+
 
